47006b569bf99e00295ed29e447f7b61d31d69c8
joserobjr <jose.rob.jr@gmail.com>
2019-05-26 18:02:02 -0300

    Documents RegionPos and adds a new constructor

--- src/main/kotlin/br/com/gamemods/regionmanipulator/RegionPos.kt
+++ src/main/kotlin/br/com/gamemods/regionmanipulator/RegionPos.kt
@@ -1,3 +1,18 @@
 package br.com.gamemods.regionmanipulator
 
-data class RegionPos(val xPos: Int, val zPos: Int)
+/**
+ * A region position extracted from the region file name.
+ *
+ * `r.-2.3.mca` must be `Region(-2,3)` for example
+ * @property xPos The first number in the region file name. May be negative.
+ * @property zPos The second number in the region file name. May be negative.
+ */
+data class RegionPos(val xPos: Int, val zPos: Int) {
+    private constructor(mcaFileNameParts: List<String>): this(mcaFileNameParts[1].toInt(), mcaFileNameParts[2].toInt())
+
+    /**
+     * Parses a region file name. Only support valid names like `r.-3.2.mca`.
+     * @param mcaFileName A valid file name
+     */
+    constructor(mcaFileName: String): this(mcaFileName.split('.'))
+}
